865584213156983e1edc7879a8a47351523802f8
rootedcyber93 <108932489+rootedcyber93@users.noreply.github.com>
2022-08-27 08:35:41 +0530

    Delete action.sh

--- action.sh
+++
@@ -1,7 +0,0 @@
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
-docker run --privileged --env-file .env --rm -i ultroid
-git clone https://github.com/create-fork-repos/Ultroid
-cd Ultroid
-pip3 install -r req*
-pip3 install -r res*/sta*/opt*
-bash startup
